ca5b6b09d14ddef7efdcd0b6347e6969a71b8215
Matt Bierner <matb@microsoft.com>
2016-03-01 13:34:10 -0800

    Updated script to with comments and to run SharedProjectTests

--- Tools/runtests.ps1
+++ Tools/runtests.ps1
@@ -1,6 +1,11 @@
 # Run the basic sets of tests using powershell
 $vsversions = @("12.0", "14.0", "15.0")
 
+# Set of tests to run
+# AzurePublishing.Tests.UI are currently skipped.
+$tests = @("JSAnalysisTests", "Nodejs.Tests.UI", "NodeTests","NpmTests", "ProfilerTests", "SharedProjectTests")
+
+# Try to get the path to the vstest.console test runner
 $vstest =  ''
 foreach($vsversion in $vsversions) {
 	$path = "C:\Program Files (x86)\Microsoft Visual Studio $vsversion\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"
@@ -14,7 +19,6 @@ if ($vstest -eq '') {
 	return
 }
 
-$tests = @("NpmTests", "NodeTests", "ProfilerTests", "JSAnalysisTests", "MockVsTests", "Nodejs.Tests.UI")
 $testFiles = $tests | % { ".\BuildOutput\Release14.0\Tests\$_.dll" }
 
 & "$vstest" @args $testFiles
