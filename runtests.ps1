bd0c15de3eb6fe8d73fb9ffadb164cb42937832e
Matt Bierner <matb@microsoft.com>
2016-03-01 13:28:39 -0800

    Try to find vstest.console for other versions of VS

--- runtests.ps1
+++ runtests.ps1
@@ -1,7 +1,20 @@
 # Run the basic sets of tests using powershell
-$vstest = "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"
+$vsversions = @("12.0", "14.0", "15.0")
+
+$vstest =  ''
+foreach($vsversion in $vsversions) {
+	$path = "C:\Program Files (x86)\Microsoft Visual Studio $vsversion\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"
+	if (Test-Path $path) {
+		$vstest = $path
+	}
+}
+
+if ($vstest -eq '') {
+	echo "Could not find valid vstest.console"
+	return
+}
 
 $tests = @("NpmTests", "NodeTests", "ProfilerTests", "JSAnalysisTests", "MockVsTests", "Nodejs.Tests.UI")
 $testFiles = $tests | % { ".\BuildOutput\Release14.0\Tests\$_.dll" }
 
-& $vstest @args $testFiles
+& "$vstest" @args $testFiles
